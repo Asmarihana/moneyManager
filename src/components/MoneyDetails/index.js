88cb8311fbb174fd323064af348c4a5881556357
Asmarihana <140911738+Asmarihana@users.noreply.github.com>
2023-11-04 21:54:09 +0530

    Update index.js

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -2,39 +2,50 @@ import './index.css'
 
 const MoneyDetails = props => {
   const {balanceAmount, incomeAmount, expensesAmount} = props
+
   return (
-    <div className="money-container">
+    <div className="money-details-container">
       <div className="balance-container">
         <img
           src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
           alt="balance"
+          className="details-img"
         />
         <div>
-          <p>Your Balance</p>
-          <p data-testid="balanceAmount">Rs {balanceAmount}</p>
+          <p className="details-text">Your Balance</p>
+          <p className="details-money" data-testid="balanceAmount">
+            Rs {balanceAmount}
+          </p>
         </div>
       </div>
       <div className="income-container">
         <img
           src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
           alt="income"
+          className="details-img"
         />
         <div>
-          <p>Your Income</p>
-          <p data-testid="incomeAmount">Rs {incomeAmount}</p>
+          <p className="details-text">Your Income</p>
+          <p className="details-money" data-testid="incomeAmount">
+            Rs {incomeAmount}
+          </p>
         </div>
       </div>
       <div className="expenses-container">
         <img
           src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
           alt="expenses"
+          className="details-img"
         />
         <div>
-          <p>Your Expenses</p>
-          <p data-testid="expensesAmount">Rs {expensesAmount}</p>
+          <p className="details-text">Your Expenses</p>
+          <p className="details-money" data-testid="expensesAmount">
+            Rs {expensesAmount}
+          </p>
         </div>
       </div>
     </div>
   )
 }
+
 export default MoneyDetails
